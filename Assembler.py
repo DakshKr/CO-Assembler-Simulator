3f5ea679d90d1a3b184a90b052ca1072cd424b44
Daksh <164284890+DakshKr@users.noreply.github.com>
2025-02-12 09:35:05 +0000

    Updated assembler logic for error handling v2

--- Assembler.py
+++ Assembler.py
@@ -10,26 +10,30 @@ def main():
 
     ...
 
-# fn to check whether the user gave valid command line arguments
+# fn to check whether the user gave valid CLIs
 def check_input():
 
     # make sure that the user gave both the input file and output file
-    if len(sys.argv) != 3:
-        sys.exit("Missing Command Line Argument")
+    if len( sys.argv ) != 3:
+        sys.exit( "\nMissing/Exceeding the number of Command Line Arguments\n\n" )
 
     # checks whether the input file name is valid
-    if not re.fullmatch(r"^.+\.txt$", sys.argv[1]):
-        sys.exit("Invalid Input File Name")
+    if not re.fullmatch( r"^.+\.txt$", sys.argv[1] ):
+        sys.exit( "\nInvalid Input File Name\n\n" )
 
     # checks whether the input file name is valid
-    if not re.fullmatch(r"^.+\.txt$", sys.argv[2]):
-        sys.exit("Invalid Output File Name")
+    if not re.fullmatch( r"^.+\.txt$", sys.argv[2] ):
+        sys.exit( "\nInvalid Output File Name\n\n" )
 
     # make sures whether the input file exists
     try:
-        f = open(f"{sys.argv[1]}", 'r')
+        f = open( f"{sys.argv[1]}", "r" )
+        f.close()
     except FileNotFoundError:
-        sys.exit("Input File Not Found")
+        sys.exit( "\nInput File Not Found\n\n" )
+
+    if sys.argv[1] == sys.argv[2]:
+        sys.exit( "\nInput & Output File Can't be same\n\n" )
 
 
 if __name__ == "__main__":
