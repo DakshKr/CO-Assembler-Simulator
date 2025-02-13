c91182d003cc2b85de969527a610a158618a39f2
Daksh <164284890+DakshKr@users.noreply.github.com>
2025-02-13 14:47:37 +0000

    First pass: extract labels and handle duplicate labels

--- Assembler.py
+++ Assembler.py
@@ -1,14 +1,30 @@
 # import files
 import sys
 import re
+import json
 
 # main
 def main():
 
     check_input()
 
+    with open( sys.argv[1], "r" ) as file:
 
-    ...
+        input_code = [ loc.split('#')[0].strip() 
+                      for loc in file
+                      if loc.split('#')[0].strip() != "" ]
+    
+    labels_dict = get_labels(input_code)
+
+    for i in labels_dict:
+        print(i, labels_dict[i])
+
+
+
+
+
+
+
 
 # fn to check whether the user gave valid CLIs
 def check_input():
@@ -35,6 +51,27 @@ def check_input():
     if sys.argv[1] == sys.argv[2]:
         sys.exit( "\nInput & Output File Can't be same\n\n" )
 
+
+# fn to return all the labels in the assembly code
+def get_labels(code):
+    labels = {}
+    pc = 0
+    for line_of_code in code:
+        if ( match := re.match( r"^(\w+)\s*:(.*)", line_of_code ) ):
+            label = match.group(1)
+            rest_of_line = match.group(2).strip()
+
+            if label in labels:
+                    sys.exit(f"Error: Duplicate label '{label}'")
+
+            labels[label] = pc
+
+            if rest_of_line: pc+=4
+
+        else:
+            pc+=4
+
+    return labels
 
 if __name__ == "__main__":
     main()
