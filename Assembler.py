414f9bf0877003e4e0444b5ddd1c7051dc277f76
Daksh <164284890+DakshKr@users.noreply.github.com>
2025-02-14 05:07:34 +0000

    Converting to binary code 1

--- Assembler.py
+++ Assembler.py
@@ -9,15 +9,13 @@ def main():
     check_input()
 
     with open( sys.argv[1], "r" ) as file:
-
-        input_code = [ loc.split('#')[0].strip() 
-                      for loc in file
+        input_code_list = [ loc.split('#')[0].strip() 
+                       for loc in file
                       if loc.split('#')[0].strip() != "" ]
     
-    labels_dict = get_labels(input_code)
+    labels_dict = get_labels( input_code_list )
 
-    for i in labels_dict:
-        print(i, labels_dict[i])
+    binary_code_list = convert_to_binary( input_code_list, labels_dict )
 
 
 
@@ -62,7 +60,7 @@ def get_labels(code):
             rest_of_line = match.group(2).strip()
 
             if label in labels:
-                    sys.exit(f"Error: Duplicate label '{label}'")
+                sys.exit(f"Error: Duplicate label '{label}'")
 
             labels[label] = pc
 
@@ -72,6 +70,61 @@ def get_labels(code):
             pc+=4
 
     return labels
+
+
+def convert_to_binary( code, labels ):
+    with open("opcodes.json", "r") as f1:
+        opcodes_dict = json.load(f1)
+
+    binary_list = []
+    line_number = 0
+    pc = 0
+
+    for line in code:
+        line_number+=1
+        
+        if ( match := re.match( r"^(\w+)\s*:(.*)", line ) ):
+            if match.group(2).strip() != "":
+                line_of_code = match.group(2).strip()
+            else:
+                continue
+        else:
+            line_of_code = line
+        
+        elements = re.split(r'[ ,()]+', line_of_code)
+        instruction = elements[0]
+        instruction_data = opcodes_dict.get(instruction)
+
+        if instruction not in opcodes_dict:
+            sys.exit(f"Error: Unknown instruction '{instruction}' at line {line_number}" )
+        
+        match instruction_data["type"]:
+
+            case "R":
+                binary_line = get_R_type_binary()
+            
+            case "I":
+                binary_line = get_I_type_binary()
+            
+            case "S":
+                binary_line = get_S_type_binary()
+
+            case "B":
+                binary_line = get_B_type_binary()
+
+            case "J":
+                binary_line = get_J_type_binary()
+            
+            case _:
+                binary_line = get_special_
+            
+
+            
+    
+
+    
+    ...
+
 
 if __name__ == "__main__":
     main()
